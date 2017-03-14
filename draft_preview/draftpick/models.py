d5e947184bcec31a615db2d3882b08f503ee38f1
cjredmond <connor.redmond@gmail.com>
2017-03-14 10:22:55 -0400

    added all players

--- draft_preview/draftpick/models.py
+++ draft_preview/draftpick/models.py
@@ -8,8 +8,21 @@ class Player(models.Model):
     take = models.TextField()
     pick = models.CharField(max_length=10)
 
+    def __str__(self):
+        return self.name
+
     @property
     def image_url(self):
         if self.picture:
             return self.picture.url
         return 'http://pccweb.ca/standrewsdresden/files/2016/03/Question-mark.png'
+
+    def next_pick(self):
+        next_pick = Player.objects.get(id=self.id + 1)
+        if next_pick:
+            return next_pick
+
+    def prev_pick(self):
+        prev_pick = Player.objects.get(id=self.id - 1)
+        if prev_pick:
+            return prev_pick
